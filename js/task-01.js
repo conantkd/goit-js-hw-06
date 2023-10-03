830d570522041f10f34e8bc0456b361cd8a704e2
C@nantk <128438309+conantkd@users.noreply.github.com>
2023-10-03 16:31:56 -0500

    Tarea 1-6

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,10 @@
-ff
+const categoriesList = document.querySelector('#categories');
+const categoriesItem=categoriesList.querySelectorAll('li.item')
+console.log('Number of Categories:',categoriesItem.length);
+categoriesItem.forEach(categorieItem => {
+    const categoryName = categorieItem.querySelector('h2').textContent;
+    const categoryItemsList = categorieItem.querySelectorAll('ul li');
+    console.log('Category:', categoryName);
+    console.log('Elements:',categoryItemsList.length);
+}
+);
