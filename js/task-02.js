b1898fed2c00fa503291d7c0b25efedff4491a1e
C@nantk <128438309+conantkd@users.noreply.github.com>
2023-10-03 21:11:11 -0500

    tarea 2-6 checked

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+  const ingredients = [
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
+  ];
+
+const ingredientsUnorganizedList = document.getElementById('ingredients');
+console.log(ingredientsUnorganizedList);
+
+    ingredients.forEach(ingredient => {
+      const ingredientList = document.createElement('li');
+      ingredientList.textContent = ingredient;
+      ingredientList.className = 'item';
+      ingredientsUnorganizedList.appendChild(ingredientList);
+    });
