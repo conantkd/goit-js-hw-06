502ffa2a1cd2d0dd2b0785c570bfa5a96c7293a8
C@nantk <128438309+conantkd@users.noreply.github.com>
2023-10-04 22:13:14 -0500

    tarea 5-6

---
+++ js/task-05.js
@@ -0,0 +1,16 @@
+const inputBox = document.querySelector('#name-input');
+const nameBox = document.querySelector("#name-output");
+
+inputBox.addEventListener('input', (event) =>
+{
+    const name = event.target.value;
+    if (name.length > 0 )    
+    {
+        nameBox.textContent = name;
+    }
+    else {
+        nameBox.textContent = "Anonymus";
+    }
+    
+}
+)
