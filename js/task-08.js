c49acb109c4ce925af604e23845a9a0b5488a4aa
C@nantk <128438309+conantkd@users.noreply.github.com>
2023-10-05 20:30:13 -0500

    task 8-6 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const emailInput = document.querySelector('[type="email"]');
+const passwordInput = document.querySelector('[type="password"]');
+const loginForm = document.querySelector("form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const emailInput = event.target.elements.email.value;
+  const passwordInput = event.target.elements.password.value;
+
+  if (emailInput.length < 1 || passwordInput.length < 1) {
+    alert(`Todos los espacios deben ser rellenados`);
+  } else {
+    console.log(`email: ${emailInput}, password: ${passwordInput}`);
+    loginForm.reset();
+  }
+});
